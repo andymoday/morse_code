d667345d8cf4b83b819559e494711163809dba38
Andy Moday <andy.mopad@gmail.com>
2021-07-02 18:00:06 +0100

    Complete basic functionality

--- main.py
+++ main.py
@@ -1,67 +1,78 @@
-morse_dict = {'a':'⚫➖',
-              'b':'➖⚫⚫⚫',
-              'c':'➖⚫➖⚫',
-              'd':'➖⚫⚫',
-              'e':'⚫',
-              'f':'⚫⚫➖⚫',
-              'g':'➖➖⚫',
-              'h':'⚫⚫⚫⚫',
-              'i':'⚫⚫',
-              'j':'⚫➖➖➖',
-              'k':'➖⚫➖',
-              'l':'⚫➖⚫⚫',
-              'm':'➖➖',
-              'n':'➖⚫',
-              'o':'➖➖➖',
-              'p':'⚫➖➖⚫',
-              'q':'➖➖⚫➖',
-              'r':'⚫➖⚫',
-              's':'⚫⚫⚫',
-              't':'➖',
-              'u':'⚫⚫➖',
-              'v':'⚫⚫⚫➖',
-              'w':'⚫➖➖',
-              'x':'➖⚫⚫➖',
-              'y':'➖⚫➖➖',
-              'z':'➖➖⚫⚫',
-              '.':'⚫➖⚫➖⚫➖',
-              ',':'➖➖⚫⚫➖➖',
-              '?':'⚫⚫➖➖⚫⚫',
-              '\'':'⚫➖➖➖➖⚫',
-              '!':'➖⚫➖⚫➖➖',
-              '/':'➖⚫⚫➖⚫',
-              '(':'➖⚫➖➖⚫',
-              ')':'➖⚫➖➖⚫➖',
-              '&':'⚫➖⚫⚫⚫',
-              ':':'➖➖➖⚫⚫⚫',
-              ';':'⚫➖⚫➖⚫➖',
-              '=':'➖⚫⚫⚫➖',
-              '+':'⚫➖⚫➖⚫',
-              '-':'➖⚫⚫⚫⚫➖',
-              '_':'⚫⚫➖➖⚫➖',
+morse_dict = {'a': '⚫➖',
+              'b': '➖⚫⚫⚫',
+              'c': '➖⚫➖⚫',
+              'd': '➖⚫⚫',
+              'e': '⚫',
+              'f': '⚫⚫➖⚫',
+              'g': '➖➖⚫',
+              'h': '⚫⚫⚫⚫',
+              'i': '⚫⚫',
+              'j': '⚫➖➖➖',
+              'k': '➖⚫➖',
+              'l': '⚫➖⚫⚫',
+              'm': '➖➖',
+              'n': '➖⚫',
+              'o': '➖➖➖',
+              'p': '⚫➖➖⚫',
+              'q': '➖➖⚫➖',
+              'r': '⚫➖⚫',
+              's': '⚫⚫⚫',
+              't': '➖',
+              'u': '⚫⚫➖',
+              'v': '⚫⚫⚫➖',
+              'w': '⚫➖➖',
+              'x': '➖⚫⚫➖',
+              'y': '➖⚫➖➖',
+              'z': '➖➖⚫⚫',
+              '.': '⚫➖⚫➖⚫➖',
+              ',': '➖➖⚫⚫➖➖',
+              '?': '⚫⚫➖➖⚫⚫',
+              '\'': '⚫➖➖➖➖⚫',
+              '!': '➖⚫➖⚫➖➖',
+              '/': '➖⚫⚫➖⚫',
+              '(': '➖⚫➖➖⚫',
+              ')': '➖⚫➖➖⚫➖',
+              '&': '⚫➖⚫⚫⚫',
+              ':': '➖➖➖⚫⚫⚫',
+              ';': '⚫➖⚫➖⚫➖',
+              '=': '➖⚫⚫⚫➖',
+              '+': '⚫➖⚫➖⚫',
+              '-': '➖⚫⚫⚫⚫➖',
+              '_': '⚫⚫➖➖⚫➖',
               '\"': '⚫➖⚫⚫➖⚫',
-              '$':'⚫⚫⚫➖⚫⚫➖',
-              '@':'⚫➖➖⚫➖⚫',
-              '0':'➖➖➖➖➖',
-              '1':'⚫➖➖➖➖',
-              '2':'⚫⚫➖➖➖',
-              '3':'⚫⚫⚫➖',
-              '4':'⚫⚫⚫⚫➖',
-              '5':'⚫⚫⚫⚫⚫',
-              '6':'➖⚫⚫⚫⚫',
-              '7':'➖➖⚫⚫⚫',
-              '8':'➖➖➖⚫⚫',
-              '9':'➖➖➖➖⚫'}
+              '$': '⚫⚫⚫➖⚫⚫➖',
+              '@': '⚫➖➖⚫➖⚫',
+              '0': '➖➖➖➖➖',
+              '1': '⚫➖➖➖➖',
+              '2': '⚫⚫➖➖➖',
+              '3': '⚫⚫⚫➖',
+              '4': '⚫⚫⚫⚫➖',
+              '5': '⚫⚫⚫⚫⚫',
+              '6': '➖⚫⚫⚫⚫',
+              '7': '➖➖⚫⚫⚫',
+              '8': '➖➖➖⚫⚫',
+              '9': '➖➖➖➖⚫',
+              ' ': '  '}
+
 # Get Input Text From User
 text_to_convert = input("Please Enter The Text You Wish To Convert: ")
 
 # Validate Input
-# Input is string so should not need conversion
+# Changing Case For Dictionary Lookup
+text_to_convert = text_to_convert.lower()
 
 # Create Empty Translation String
 translation = ''
-# Look up each letter in Morse Code Dictionary and add to Translation String
 
-# Return Morse Code Translation to User
+try:
+    # Look up each letter in Morse Code Dictionary and add to Translation String
+    for letter in text_to_convert:
+        translation += f"{morse_dict[letter]} "
+
+    # Return Morse Code Translation to User
+    print(translation)
+
+except KeyError:
+    print('One or more letters are not translatable. Please enter A-Z, a-z, 0-9 [.,?\'!/()&:;=+-_\"$@]')
 
 
