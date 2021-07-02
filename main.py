a6c86459557a35b27ba5956a33320aeed82e8dbc
Andy Moday <andy.mopad@gmail.com>
2021-07-02 16:58:00 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# Get Input Text From User
+text_to_convert = input("Please Enter The Text You Wish To Convert: ")
+
+# Validate Input
+
+# Create Empty Translation String
+
+# Look up each letter in Morse Code Dictionary and add to Translation String
+
+# Return Morse Code Translation to User
+
+
+
